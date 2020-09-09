3f124e0a6ce230ba6c7f7701db2126139c2646a7
DAleksandrov <dd.aleksandrov@gmail.com>
2020-09-09 17:36:23 +0300

    Remove hello world!

--- src/main/java/program/Program.java
+++ src/main/java/program/Program.java
@@ -2,10 +2,6 @@ package program;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+
     }
 }
